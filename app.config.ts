c252d72c7a14b63b15497c1da3fdbefffdd4007b
LucasMedici <lucasrpmedici@gmail.com>
2025-11-27 20:49:34 -0300

    feat: building app configs stuff

--- app.config.ts
+++ app.config.ts
@@ -4,8 +4,14 @@ export default {
   expo: {
     name: "finance-assistent-frontend",
     slug: "finance-assistent-frontend",
+    android: {
+      package: "com.lucasmedici.financeassistant",
+    },
     extra: {
       API_URL: process.env.API_URL,
+      eas: {
+        projectId: "50b9f938-af3f-4561-aaff-310292b391e3",
+      },
     },
   },
 };
