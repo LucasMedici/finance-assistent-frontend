46271fb78069c29711f5813f950fc427bd6ef926
LucasMedici <lucasrpmedici@gmail.com>
2025-11-27 21:41:46 -0300

    fix: correct message offline queue name for backend

--- src/global/offlineQueue.ts
+++ src/global/offlineQueue.ts
@@ -1,10 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { api } from '../services/api';
 
-export async function addToOfflineQueue(msg: any) {
+export async function addToOfflineQueue(userMessage: any) {
   const raw = await AsyncStorage.getItem('offlineQueue');
   const queue = raw ? JSON.parse(raw) : [];
-  queue.push(msg);
+  queue.push(userMessage);
   await AsyncStorage.setItem('offlineQueue', JSON.stringify(queue));
 }
 
@@ -14,10 +14,10 @@ export async function processOfflineQueue() {
 
   if (queue.length === 0) return;
 
-  for (const msg of queue) {
+  for (const userMessage of queue) {
     try {
-      await api.post(`/webhook/messages`, { msg });
-      await removeFromQueue(msg.id);
+      await api.post(`/webhook/messages`, { userMessage });
+      await removeFromQueue(userMessage.id);
     } catch (error) {
       console.log('Erro ao processar fila OFFLINE: ', error);
       break;
@@ -29,6 +29,6 @@ async function removeFromQueue(id: any) {
   const raw = await AsyncStorage.getItem('offlineQueue');
   const queue = raw ? JSON.parse(raw) : [];
 
-  const newQueue = queue.filter((msg: any) => msg.id !== id);
+  const newQueue = queue.filter((userMessage: any) => userMessage.id !== id);
   await AsyncStorage.setItem('offlineQueue', JSON.stringify(newQueue));
 }
